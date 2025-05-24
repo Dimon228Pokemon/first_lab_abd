6ec948dd701fd093504bef77b6dd3743e2c1860b
Dmitry Bychkov <fredmydisk@yandex.ru>
2025-05-24 06:48:35 +0300

    add Join

--- DML/add_data_dim.sql
+++ DML/add_data_dim.sql
@@ -32,78 +32,114 @@ SELECT DISTINCT product_brand FROM raw_data;
 INSERT INTO dim_materials (material_name)
 SELECT DISTINCT product_material FROM raw_data;
 
--- Вставляем города (связь с странами)
+-- Вставляем города (связь с странами через JOIN)
 INSERT INTO dim_cities (city_name, state, country_id)
 SELECT DISTINCT 
-    store_city, 
-    store_state, 
-    (SELECT country_id FROM dim_countries WHERE country_name = store_country)
-FROM raw_data
-WHERE store_city IS NOT NULL;
+    r.store_city, 
+    r.store_state, 
+    c.country_id
+FROM 
+    raw_data r
+JOIN 
+    dim_countries c ON r.store_country = c.country_name
+WHERE 
+    r.store_city IS NOT NULL;
 
--- Вставляем поставщиков
+-- Вставляем поставщиков 
 INSERT INTO dim_suppliers (supplier_name, contact_person, email, phone, address, city_id)
 SELECT DISTINCT 
-    supplier_name,
-    supplier_contact,
-    supplier_email,
-    supplier_phone,
-    supplier_address,
-    (SELECT city_id FROM dim_cities WHERE city_name = supplier_city LIMIT 1)
-FROM raw_data;
+    r.supplier_name,
+    r.supplier_contact,
+    r.supplier_email,
+    r.supplier_phone,
+    r.supplier_address,
+    c.city_id
+FROM 
+    raw_data r
+JOIN 
+    dim_cities c ON r.supplier_city = c.city_name
+WHERE 
+    r.supplier_name IS NOT NULL;
 
--- Вставляем магазины
+-- Вставляем магазины 
 INSERT INTO dim_stores (store_name, location, city_id, phone, email)
 SELECT DISTINCT 
-    store_name,
-    store_location,
-    (SELECT city_id FROM dim_cities WHERE city_name = store_city LIMIT 1),
-    store_phone,
-    store_email
-FROM raw_data;
+    r.store_name,
+    r.store_location,
+    c.city_id,
+    r.store_phone,
+    r.store_email
+FROM 
+    raw_data r
+JOIN 
+    dim_cities c ON r.store_city = c.city_name
+WHERE 
+    r.store_name IS NOT NULL;
 
--- Вставляем продавцов
+-- Вставляем продавцов 
 INSERT INTO dim_sellers (first_name, last_name, email, postal_code, country_id)
 SELECT DISTINCT 
-    seller_first_name,
-    seller_last_name,
-    seller_email,
-    seller_postal_code,
-    (SELECT country_id FROM dim_countries WHERE country_name = seller_country)
-FROM raw_data;
+    r.seller_first_name,
+    r.seller_last_name,
+    r.seller_email,
+    r.seller_postal_code,
+    c.country_id
+FROM 
+    raw_data r
+JOIN 
+    dim_countries c ON r.seller_country = c.country_name
+WHERE 
+    r.seller_first_name IS NOT NULL;
 
 -- Вставляем покупателей
 INSERT INTO dim_customers (first_name, last_name, age, email, postal_code, pet_type_id, pet_name, pet_breed_id, country_id)
 SELECT DISTINCT 
-    customer_first_name,
-    customer_last_name,
-    customer_age,
-    customer_email,
-    customer_postal_code,
-    (SELECT pet_type_id FROM dim_pet_types WHERE pet_type_name = customer_pet_type),
-    customer_pet_name,
-    (SELECT pet_breed_id FROM dim_pet_breeds WHERE pet_breed_name = customer_pet_breed),
-    (SELECT country_id FROM dim_countries WHERE country_name = customer_country)
-FROM raw_data;
+    r.customer_first_name,
+    r.customer_last_name,
+    r.customer_age,
+    r.customer_email,
+    r.customer_postal_code,
+    pt.pet_type_id,
+    r.customer_pet_name,
+    pb.pet_breed_id,
+    c.country_id
+FROM 
+    raw_data r
+JOIN 
+    dim_countries c ON r.customer_country = c.country_name
+JOIN 
+    dim_pet_types pt ON r.customer_pet_type = pt.pet_type_name
+JOIN 
+    dim_pet_breeds pb ON r.customer_pet_breed = pb.pet_breed_name
+WHERE 
+    r.customer_first_name IS NOT NULL;
 
--- Вставляем продукты
+-- Вставляем продукты 
 INSERT INTO dim_products (
     product_name, category_id, price, weight, color_id, size, brand_id, material_id, 
     description, rating, reviews, release_date, expiry_date, supplier_id
 )
 SELECT DISTINCT 
-    product_name,
-    (SELECT category_id FROM dim_product_categories WHERE category_name = product_category),
-    product_price,
-    product_weight,
-    (SELECT color_id FROM dim_colors WHERE color_name = product_color),
-    product_size,
-    (SELECT brand_id FROM dim_brands WHERE brand_name = product_brand),
-    (SELECT material_id FROM dim_materials WHERE material_name = product_material),
-    product_description,
-    product_rating,
-    product_reviews,
-    product_release_date::DATE,
-    product_expiry_date::DATE,
-    (SELECT supplier_id FROM dim_suppliers WHERE supplier_name = raw_data.supplier_name LIMIT 1)
-FROM raw_data;
+    r.product_name,
+    pc.category_id,
+    r.product_price,
+    r.product_weight,
+    cl.color_id,
+    r.product_size,
+    b.brand_id,
+    m.material_id,
+    r.product_description,
+    r.product_rating,
+    r.product_reviews,
+    r.product_release_date::DATE,
+    r.product_expiry_date::DATE,
+    s.supplier_id
+FROM 
+    raw_data r
+LEFT JOIN dim_product_categories pc ON r.product_category = pc.category_name
+LEFT JOIN dim_colors cl ON r.product_color = cl.color_name
+LEFT JOIN dim_brands b ON r.product_brand = b.brand_name
+LEFT JOIN dim_materials m ON r.product_material = m.material_name
+LEFT JOIN dim_suppliers s ON r.supplier_name = s.supplier_name
+WHERE 
+    r.product_name IS NOT NULL;
--- DML/add_data_fact.sql
+++ DML/add_data_fact.sql
@@ -1,13 +1,16 @@
 -- Вставляем продажи
 INSERT INTO fact_sales (sale_date, customer_id, seller_id, product_id, store_id, quantity, total_price)
 SELECT 
-    sale_date::DATE,
-    (SELECT customer_id FROM dim_customers 
-     WHERE first_name = customer_first_name AND last_name = customer_last_name LIMIT 1),
-    (SELECT seller_id FROM dim_sellers 
-     WHERE first_name = seller_first_name AND last_name = seller_last_name LIMIT 1),
-    (SELECT product_id FROM dim_products WHERE product_name = raw_data.product_name LIMIT 1),
-    (SELECT store_id FROM dim_stores WHERE store_name = raw_data.store_name LIMIT 1),
-    sale_quantity,
-    sale_total_price
-FROM raw_data;
+    r.sale_date::DATE,
+    c.customer_id,
+    s.seller_id,
+    p.product_id,
+    st.store_id,
+    r.sale_quantity,
+    r.sale_total_price
+FROM 
+    raw_data r
+JOIN dim_customers c ON r.customer_first_name = c.first_name AND r.customer_last_name = c.last_name
+JOIN dim_sellers s ON r.seller_first_name = s.first_name AND r.seller_last_name = s.last_name
+JOIN dim_products p ON r.product_name = p.product_name
+JOIN dim_stores st ON r.store_name = st.store_name;
